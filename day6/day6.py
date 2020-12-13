f65e24ad63a319dded3152484387cd1c4d7e5006
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2020-12-12 20:40:14 -0500

    Refactor to use set unions. This is setup for Part 2.

--- day6/day6.py
+++ day6/day6.py
@@ -4,19 +4,28 @@ from typing import List, Set
 
 def main():
 
-    answers: List[str]
+    answers: List[List[str]]
     with open('input.txt') as input_file:
         # Each group is separated by two newlines, so we split up into a list
         # of strings, with each string being a full group's answers.
         # Each person within a group has their answer separated by one newline,
-        # after splitting by group, we remove single newlines.
-        answers = [s.replace('\n', '') for s in re.split(r'\n{2}', input_file.read())]
+        # so after splitting by group, we split by person.
+        # 
+        #Example: 
+        #   answers[i][j] is the answers for the jth person in the jth group.
+        groups = re.split(r'\n{2}', input_file.read())
+
+    answers = [group.split('\n') for group in groups]
+
+    print(answers[0:10])
 
     # Convert each combined group answer string into a set of the characters.
-    answer_sets: List[Set[str]] = [set(answer) for answer in answers]
+    group_unions: List[Set[str]] = []
+    for group in answers:
+        group_unions +=  set.union(*[set(person) for person in group])
 
     # Obtain the sum of the number of characters in each set of group answers.
-    solution = sum(len(answer_set) for answer_set in answer_sets)
+    solution = sum(len(union) for union in group_unions)
     
     print(solution)
     
