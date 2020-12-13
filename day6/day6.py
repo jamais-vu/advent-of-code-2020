185f6caf09c76e5c8367dd0f708042b2fa93dfb8
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2020-12-12 20:46:56 -0500

    Completed Day 6 Part 2.

--- day6/day6.py
+++ day6/day6.py
@@ -4,7 +4,6 @@ from typing import List, Set
 
 def main():
 
-    answers: List[List[str]]
     with open('input.txt') as input_file:
         # Each group is separated by two newlines, so we split up into a list
         # of strings, with each string being a full group's answers.
@@ -13,22 +12,28 @@ def main():
         # 
         #Example: 
         #   answers[i][j] is the answers for the jth person in the jth group.
-        groups = re.split(r'\n{2}', input_file.read())
+        groups: List[str] = re.split(r'\n{2}', input_file.read())
 
-    answers = [group.split('\n') for group in groups]
+    answers: List[List[str]] = [group.split('\n') for group in groups]
 
-    print(answers[0:10])
-
-    # Convert each combined group answer string into a set of the characters.
     group_unions: List[Set[str]] = []
+    group_intersections: List[Set[str]] = []
     for group in answers:
-        group_unions +=  set.union(*[set(person) for person in group])
+        group_answers_as_sets = [set(person) for person in group]
+        group_unions +=  set.union(*group_answers_as_sets)
+        group_intersections += set.intersection(*group_answers_as_sets)
 
     # Obtain the sum of the number of characters in each set of group answers.
-    solution = sum(len(union) for union in group_unions)
+    solution_1: int = sum(len(union) for union in group_unions)
+    solution_2: int = sum(len(intersection) for intersection in group_intersections)
     
-    print(solution)
+    s1: str = f'Part 1: The sum of the counts is {solution_1}.'
+    s2: str = f'Part 2: The sum of the counts is {solution_2}.'
     
+    print(f'{s1}\n{s2}')
+
+    with open('solution.txt', mode='w') as output_file:
+        output_file.write(f'{s1}\n{s2}')
 
 if __name__ == '__main__':
     main()
