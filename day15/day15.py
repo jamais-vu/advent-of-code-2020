5672376ed881da6d595446244b3c1d6b569bf4f2
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2020-12-19 23:03:59 -0500

    Refactor code to be more clear. Remove unused functions.

--- day15/day15.py
+++ day15/day15.py
@@ -3,29 +3,25 @@ from typing import Dict, List
 def main():
 
     # Numbers spoken, where the number at index `i` was spoken on turn `i+1`.
-    starting_numbers: List[int] = []
+    numbers: List[int] = []
 
     with open('input.txt') as input_file:
-        starting_numbers = [int(x) for x in input_file.read().split(',')]
+        numbers = [int(x) for x in input_file.read().split(',')]
 
-    ##########
-    # Part 1 #
-    ##########
-    # If we don't copy starting_numbers then it will be modified for part 2.
-    numbers: List[int] = starting_numbers.copy()
-    while len(numbers) < 2020:
-        numbers += [next_number(numbers)]
-
-    solution_1: int = numbers[-1]
+    solution_1: int = nth_number_spoken(numbers, 2020)
     s1: str = f'Part 1: The 2020th number spoken is {solution_1}.'
     print(s1)
 
-    ##########
-    # Part 2 #
-    ##########
-    # Dict here is kinda slow. For larger n I'd seek a faster solution.
-    numbers: List[int] = starting_numbers.copy()
-    
+    solution_2: int = nth_number_spoken(numbers, 30000000)
+    s2: str = f'Part 2: The 30000000th number spoken is {solution_2}.'
+    print(s2)
+
+    with open('solution.txt', mode='w') as output_file:
+        output_file.write(f'Part 1: {solution_1}\nPart 2: {solution_2}')
+
+
+def nth_number_spoken(numbers: List[int], n: int) -> int:
+    """Given a list of starting numbers, finds the nth number spoken."""
     turn: int = len(numbers)
     most_recent_number: int = numbers[-1]
 
@@ -38,7 +34,7 @@ def main():
     }
 
     previous_turn: int
-    while turn < 30000000:
+    while turn < n:
         if most_recent_number in previously_spoken:
             # The turn on which the most recent number was previously spoken.
             previous_turn = previously_spoken[most_recent_number]
@@ -53,36 +49,7 @@ def main():
 
         turn += 1
 
-    print(turn)
-    print(most_recent_number)
-    # print(last_spoken)
-
-
-def next_number(numbers: List[int]) -> int:
-    """Determines the next number to be spoken."""
-    current_turn: int = len(numbers)
-    most_recent_number: int = numbers[-1]
-
-    if most_recent_number not in numbers[0:-1]:
-        # If the last number was not spoken before, the next number is 0.
-        return 0
-    else:
-        # We use numbers[:-1] to exclude the most recent number, since we want 
-        # the index where it was spoken before that.
-        turn_last_spoken: int = most_recent_index(numbers[:-1], most_recent_number) + 1
-        return current_turn - turn_last_spoken
-
-
-def most_recent_index(numbers: List[int], n: int) -> int:
-    """Returns the greatest index in numbers which contains n.
-
-    Raises ValueError if n is not in the given list.
-    """
-    for i in reversed(range(len(numbers))):
-        if n == numbers[i]:
-            return i
-
-    raise ValueError(f'{n} is not in numbers.')
+    return most_recent_number
 
 
 if __name__ == '__main__':
