dfc21c84b411be256cee94e8a5e71e1a441d370a
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2020-12-21 19:52:18 -0500

    Constraint solver works for example_2 but not input

--- day16/day16.py
+++ day16/day16.py
@@ -1,13 +1,14 @@
 import itertools
 import re
 
+from collections import defaultdict
 from typing import Dict, Iterable, List
 
 
 def main():
 
     notes: List[str] = []
-    with open('input.txt') as input_file:
+    with open('example_1.txt') as input_file:
         notes = input_file.read().split('\n\n')
 
     fields: List[str] = notes[0].split('\n')
@@ -28,18 +29,72 @@ def main():
     nearby_tickets: List[List[int]] = [
         get_ticket_values(ticket) for ticket in notes[2].split('\n')[1:]]
     
-    # print(fields, your_ticket, nearby_tickets)
-
     # A list of all valid ranges, regardless of field.
     all_valid_ranges: List[Iterable[int]] = list(
         itertools.chain.from_iterable(ranges.values()))
     
+    # A list of tickets which have no invalid values
+    valid_tickets: List[List[int]] = []
+
     sum_of_invalid_values: int = 0
     for ticket in nearby_tickets:
-        sum_of_invalid_values += is_valid_ticket(ticket, all_valid_ranges)
+        invalid_values = is_valid_ticket(ticket, all_valid_ranges)
+        sum_of_invalid_values += invalid_values
+        if invalid_values == 0:
+            # The ticket is valid if its invalid values are 0.
+            valid_tickets += [ticket]
 
     s1: str = f'Part 1: The sum of invalid values is {sum_of_invalid_values}.'
     print(s1)
+
+    # Part 2 
+
+    print(ranges.keys())
+    print(valid_tickets)
+
+    number_of_fields: int = len(fields)
+    number_of_valid_tickets: int = len(valid_tickets)
+
+    # Dict mapping column number to all valid field names for that column.
+    column_to_field: Dict[int, List[str]] = defaultdict(list)
+    for field_name, valid_ranges in ranges.items():
+        # print('field_name:   ', field_name)
+        # print('valid_ranges: ', valid_ranges)
+        for column in range(number_of_fields):
+            # print('column number: ', column)
+            # print([ticket[column] for ticket in valid_tickets])
+            if sum(is_valid_value(ticket[column], valid_ranges) for ticket in valid_tickets) == number_of_valid_tickets:
+                column_to_field[column] += [field_name]
+                # print(f'column {column} is {field_name}')
+            # else:
+                # print(f'column {column} is NOT {field_name}')
+
+    # List of fields for which there is only one column of valid values.
+    set_fields: List[str] = [v[0] for v in column_to_field.values() if len(v) == 1]
+    # print(column_to_field)
+    # print(set_fields)
+
+    # What we have is some columns which can only be one field. We consider 
+    # these fields to be "set"; they cannot be any other column.
+    # Since those columns are set to those fields, we remove those field names 
+    # from the other columns, and check again which columns can only be one 
+    # field. We repeat this process until each column can only be one field.
+    # (This is some sort of contraint problem but I don't know the name.)
+    i: int = 0
+    while len(set_fields) < number_of_fields:
+        set_field = set_fields[i]
+        for column, fields in column_to_field.items():
+            if set_field in fields and len(fields) != 1:
+                column_to_field[column].remove(set_field)
+            if set_field not in fields and len(fields) == 1:
+                set_fields += [fields[0]]
+        i += 1
+
+    print(set_fields)
+    print(column_to_field)
+
+    columns_as_str: str = '\n'.join(sorted(f'{column}: {field_name}' for column, field_name in column_to_field.items()))
+    print(columns_as_str)
 
 
 def get_field_ranges(s: str) -> List[Iterable[int]]:
