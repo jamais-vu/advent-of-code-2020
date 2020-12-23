0db4b6eb468ad10e5a2287ffd8e45c60b8d48a06
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2020-12-22 19:59:10 -0500

    Completed Day 7 Part 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,9 +1,7 @@
-import pprint
 import re
-import traceback
 
 from collections import defaultdict
-from typing import DefaultDict, List, Set
+from typing import DefaultDict, Dict, Iterable, List, Set
 
 def main():
 
@@ -30,24 +28,63 @@ def main():
     for bag in bags.keys():
         path_exists[bag] = find_path(bags, bag, 'shiny gold')
 
-    # print('path_exists:', pprint.pformat(path_exists), end='\n\n')
-
     valid_paths = [path for path in path_exists.values() if path is not None] 
-    # print('valid paths:\n', pprint.pformat(valid_paths))
 
     # We subtract 1 from the answer because `['shiny gold']` is a valid path,
     # but we want only paths which start with a different color bag.
-    print(f'[\'shiny gold\'] in valid_paths: {["shiny gold"] in valid_paths}')
-    print('number of valid paths: ', len(valid_paths) - 1)   
+    solution_1: str = f'Part 1: {len(valid_paths) - 1}.'
+    print(solution_1)
+
+    ##########
+    # Part 2 #
+    ##########
+    # Outer dict mapping bag colors to an inner dict of contained bags and their
+    # quantity.
+    bags: DefaultDict[str, Dict[str, int]] = defaultdict(dict)
+    for line in input_text:
+        # Split up the line into a List[str] of the outer bag, followed by
+        # the number and colors of bags it contains, if any.
+        split_line = list(filter(None, re.split(r' bags?[,.]? ?|contain ', line)))
+        outer_bag = split_line[0]
+
+        # We skip if the container bag contains no bags.
+        # if split_line[1] == 'no other'
+        if split_line[1] != 'no other':            
+            for bag in split_line[1:]:
+                # Split up the string into number and color of contained bag.
+                number, inner_bag = re.split(r'(?<=\d) ', bag)   
+                # We merge the two dicts             
+                bags[outer_bag] = {**bags[outer_bag], **{inner_bag:int(number)}}
+
+    explored: List[str] = [] # List of all bags nested within shiny gold
+    queue: List[str] = [] # List of bags we check nestings of
+    color: str
+    quantity: int
+    for color, quantity in bags['shiny gold'].items():
+        queue += [color] * quantity
+
+    while queue:
+        bag: str = queue.pop()
+        explored.append(bag)
+        for color, quantity in bags[bag].items():
+            queue += [color] * quantity
+
+    solution_2: str = f'Part 2: {len(explored)}.'
+    print(solution_2)
+
+    with open('solution.txt', mode='w') as output_file:
+        output_file.write(f'{solution_1}\n{solution_2}')
+
 
 def find_path(
-    graph: DefaultDict[str, Set[str]], 
+    graph: Dict[str, Iterable[str]], 
     start_node: str, 
     end_node: str, 
     path: List[str] = [], 
     indent: int = -1, 
-    debug: bool = False):
-    """TODO"""
+    debug: bool = False
+    ) -> List[str]:
+    """Finds a path from the start node to the end node in the given graph."""
     indent = indent + 1
     spaces = '  ' * indent
     if debug:
@@ -84,47 +121,6 @@ def find_path(
                 if debug:
                     print(f'{spaces}\tnode "{node}" in path\n\treturning None', end='\n\n')
                 return None
-
-
-# def find_paths(
-#     bags: DefaultDict[str, Set[str]], 
-#     color: str,
-#     path: List[str] = []
-#     ) -> List[str]:
-#     """
-#     Recursive method of finding all possible nestings of bags containing given color.
-
-#     TODO
-#     """
-#     print('\ncalled `find_paths()`')
-#     print('color: ', color)
-#     print('path: ', path)
-
-#     path += [color]
-#     if color not in bags:
-#         print('color NOT in bags: ', color)
-#         return path    
-#     else:
-#         print(f'color \'{color}\' is contained in {bags[color]}')
-
-#         paths = []
-
-#         for container_bag_color in bags[color]:
-#             print('container_bag_color: ', container_bag_color)
-            
-#             if color not in path:
-
-#                 try:
-#                     paths += find_paths(bags, container_bag_color, path, paths)
-                
-#                 except TypeError as e:
-#                     print(f'\n{traceback.format_exc()}\nVALUES:')
-#                     print('bags: ', pprint.pformat(bags))
-#                     print('container_bag_color: ', container_bag_color)
-#                     print('path: ', path)
-#                     print('paths: ', pprint.pformat(paths))        
-        
-#         return paths
 
 
 if __name__ == '__main__':
